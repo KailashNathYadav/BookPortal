672a1bbcc7d3f0dcada313436bd286b7fb726a7b
Kailash Nath Yadav <107490624+KailashNathYadav@users.noreply.github.com>
2024-06-10 20:36:47 +0530

    Book Portal Web Application Using PHP &  MySQL.

--- index.php
+++ index.php
@@ -1,8 +1,4 @@
 <?php
-// Store insert,update,delete for showing alert
-$insert = false;
-$update = false;
-$delete = false;
 // Connect to the Database 
 $servername = "localhost";
 $username = "root";
@@ -14,14 +10,6 @@ $conn = mysqli_connect($servername, $username, $password, $database);
 if (!$conn) {
     die("Sorry we failed to connect: " . mysqli_connect_error());
 }
-
-if (isset($_GET['delete'])) {
-    $id = $_GET['delete'];
-    $delete = true;
-    $sql = "DELETE FROM `books` WHERE `Id` = $id";
-    $result = mysqli_query($conn, $sql);
-}
-
 if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     if (isset($_POST['idEdit'])) {
         // Update the Book Details
@@ -30,32 +18,20 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
         $author = $_POST["authorEdit"];
         $isbn = $_POST["isbnEdit"];
         $publishedDate = $_POST["publishedDateEdit"];
-
+        $description = $_POST["descriptionEdit"];
         // Sql query to be executed
-        $sql = "UPDATE `books` SET `Title` = '$title', `Author` = '$author', `ISBN` = '$isbn', `PublishedDate` = '$publishedDate' WHERE `books`.`Id` = $id";
+        if($id)
+            $sql = "UPDATE `books` SET `Title` = '$title', `Author` = '$author', `ISBN` = '$isbn', `PublishedDate` = '$publishedDate', `Description` = '$description' WHERE `books`.`Id` = $id";
+        else 
+            $sql = "INSERT INTO `books` (`Title`, `Author`, `ISBN`, `PublishedDate`,`Description`) VALUES ('$title', '$author', '$isbn', '$publishedDate','$description')";
         $result = mysqli_query($conn, $sql);
-        if ($result) {
-            $update = true;
-        } else {
-            echo "We could not update the record successfully";
-        }
-    } else {
-        // Create the Book Details
-        $title = $_POST["title"];
-        $author = $_POST["author"];
-        $isbn = $_POST["isbn"];
-        $publishedDate = $_POST["publishedDate"];
-
-        // Sql query to be executed
-        $sql = "INSERT INTO `books` (`Title`, `Author`, `ISBN`, `PublishedDate`) VALUES ('$title', '$author', '$isbn', '$publishedDate')";
-        $result = mysqli_query($conn, $sql);
-
-        if ($result) {
-            $insert = true;
-        } else {
-            echo "The record was not inserted successfully because of this error ---> " . mysqli_error($conn);
-        }
-    }
+    } 
+}
+if (isset($_GET['delete'])) {
+    $id = $_GET['delete'];
+    $delete = true;
+    $sql = "DELETE FROM `books` WHERE `Id` = $id";
+    $result = mysqli_query($conn, $sql);
 }
 ?>
 
@@ -71,14 +47,14 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
 </head>
 
 <body>
-    <!-- Edit Modal -->
+    <!-- Create/Edit/Show Modal -->
     <div class="modal fade" id="editModal" tabindex="-1" role="dialog" aria-labelledby="editModalLabel" aria-hidden="true">
         <div class="modal-dialog" role="document">
             <div class="modal-content">
                 <div class="modal-header">
-                    <h5 class="modal-title" id="editModalLabel">Edit this Book</h5>
+                <h5 class="modal-title" id="editModalLabel">Book Details</h5>
                     <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                        <span aria-hidden="true">×</span>
+                        <span aria-hidden="true"></span>
                     </button>
                 </div>
                 <form action="/cruds/index.php" method="POST">
@@ -99,6 +75,10 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
                         <div class="form-group">
                             <label for="publishedDate">Published Date</label>
                             <input type="date" class="form-control" id="publishedDateEdit" name="publishedDateEdit">
+                        </div>
+                        <div class="form-group">
+                            <label for="description">Description</label>
+                            <input type="text" class="form-control" id="descriptionEdit" name="descriptionEdit">
                         </div>
                     </div>
                     <div class="modal-footer d-block mr-auto">
@@ -122,6 +102,9 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
                     <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
                 </li>
                 <li class="nav-item">
+                    <a class="nav-link" id="add" style="cursor:pointer">Add Book</a>
+                </li>
+                <li class="nav-item">
                     <a class="nav-link" href="#">About</a>
                 </li>
                 <li class="nav-item">
@@ -131,68 +114,10 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
         </div>
     </nav>
 
-    <div>
-        <?php
-        if ($insert) {
-            echo "<div class='alert alert-success alert-dismissible fade show' role='alert'>
-            <strong>Success!</strong> Your book has been inserted successfully !!!!!!!!!
-            <button type='button' class='close' data-dismiss='alert' aria-label='Close'>
-            <span aria-hidden='true'>×</span>
-            </button>
-            </div>";
-        }
-        ?>
-        <?php
-        if ($delete) {
-            echo "<div class='alert alert-success alert-dismissible fade show' role='alert'>
-            <strong>Success!</strong> Your book has been deleted successfully !!!!!!!!
-            <button type='button' class='close' data-dismiss='alert' aria-label='Close'>
-            <span aria-hidden='true'>×</span>
-            </button>
-            </div>";
-        }
-        ?>
-        <?php
-        if ($update) {
-            echo "<div class='alert alert-success alert-dismissible fade show' role='alert'>
-            <strong>Success!</strong> Your book has been updated successfully !!!!!!!!
-            <button type='button' class='close' data-dismiss='alert' aria-label='Close'>
-            <span aria-hidden='true'>×</span>
-            </button>
-            </div>";
-        }
-        ?>
-    </div>
 
-    <!-- Add Book Section -->
+    <!-- Table for book details -->
     <div class="container my-4">
-        <form action="/cruds/index.php" method="POST">
-            <div class="d-flex justify-content-between">
-                <div class="form-group w-100 mx-2">
-                    <label for="title">Book Title</label>
-                    <input type="text" class="form-control" id="title" name="title">
-                </div>
-                <div class="form-group w-100 mx-2">
-                    <label for="author">Author</label>
-                    <input type="text" class="form-control" id="author" name="author">
-                </div>
-            </div>
-            <div class="d-flex justify-content-between">
-                <div class="form-group w-100 mx-2">
-                    <label for="isbn">ISBN</label>
-                    <input type="text" class="form-control" id="isbn" name="isbn">
-                </div>
-                <div class="form-group w-100 mx-2">
-                    <label for="publishedDate">Published Date</label>
-                    <input type="date" class="form-control" id="publishedDate" name="publishedDate">
-                </div>
-            </div>
-            <button type="submit" class="btn btn-primary mx-2 w-25">Add Book</button>
-        </form>
-    </div>
-
-    <div class="container my-4">
-        <table class="table display" id="myTable">
+        <table class="table display ui celled" id="myTable" style="width: 100%;">
             <thead>
                 <tr>
                     <th scope="col">ID</th>
@@ -200,6 +125,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
                     <th scope="col">Author</th>
                     <th scope="col">ISBN</th>
                     <th scope="col">Published Date</th>
+                    <th scope="col" style='display:none'>Description</th>
                     <th scope="col">Actions</th>
                 </tr>
             </thead>
@@ -213,10 +139,11 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
                     echo
                     "<tr>
                         <th scope='row'>" . $id . "</th>
-                        <td>" . $row['Title'] . "</td>
+                        <td id=" . $row['Id'] . " class='edit' style='cursor:pointer'>" . $row['Title'] . "</td>
                         <td>" . $row['Author'] . "</td>
                         <td>" . $row['ISBN'] . "</td>
                         <td>" . $row['PublishedDate'] . "</td>
+                        <td style='display:none'>" . $row['description'] . "</td>
                         <td>
                          <button class='edit btn btn-sm btn-primary' id=" . $row['Id'] . ">Edit</button>
                          <button class='delete btn btn-sm btn-danger' id=d" . $row['Id'] . ">Delete</button>
@@ -228,6 +155,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
         </table>
     </div>
     <hr>
+
     <!-- jQuery, then Bootstrap JS,then DataTable JS, then custom JS -->
     <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
@@ -237,25 +165,41 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
             $('#myTable').DataTable();
         });
 
+        const add = document.getElementById('add');
+        add.addEventListener("click",(e)=>{
+            $('#editModal').modal('toggle');
+        });
+
         edits = document.getElementsByClassName('edit');
         Array.from(edits).forEach((element) => {
             element.addEventListener("click", (e) => {
-                console.log("edit ");
                 tr = e.target.parentNode.parentNode;
                 title = tr.getElementsByTagName("td")[0].innerText;
                 author = tr.getElementsByTagName("td")[1].innerText;
                 isbn = tr.getElementsByTagName("td")[2].innerText;
                 publishedDate = tr.getElementsByTagName("td")[3].innerText;
-                console.log(title, author, isbn, publishedDate);
+                description = tr.getElementsByTagName("td")[4].innerText;
+
                 titleEdit.value = title;
                 authorEdit.value = author;
                 isbnEdit.value = isbn;
                 publishedDateEdit.value = publishedDate;
+                descriptionEdit.value = description;
+
                 idEdit.value = e.target.id;
-                console.log(e.target.id);
+
                 $('#editModal').modal('toggle');
+                clearDetails();
             });
         });
+
+        const clearDetails = ()=>{
+            titleEdit.value = title;
+            authorEdit.value = author;
+            isbnEdit.value = isbn;
+            publishedDateEdit.value = publishedDate;
+            descriptionEdit.value = description;
+        }
 
         deletes = document.getElementsByClassName('delete');
         Array.from(deletes).forEach((element) => {
@@ -265,7 +209,6 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
 
                 if (confirm("Are you sure you want to delete this book?")) {
                     console.log("yes");
-                    
                     window.location = `/cruds/index.php?delete=${id}`;
                 } else {
                     console.log("no");
